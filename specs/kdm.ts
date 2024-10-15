4439360489f86cf14c2d4884c32fd4392c296ad4
Shigu <shiguww@gmail.com>
2024-10-15 18:39:46 +0400

    Add back the tests

---
+++ specs/kdm.ts
@@ -0,0 +1,25 @@
+import KDM from "#/kdm/kdm";
+import crypto from "node:crypto";
+import fs from "node:fs/promises";
+import { test, expect, describe } from "vitest";
+
+function expectSHA256Checksum(data: Buffer, file: string, checksum: string): void {
+  const sum = crypto.createHash("sha256")
+    .update(data).digest("hex");
+
+  expect(sum).to.be.equal(checksum, `Invalid ${file} checksum`);
+}
+
+describe("KDM", () => {
+  describe("kdm_mapdata.bin", () => {
+    test("parsing and building yields the same file", async () => {
+      const data = await fs.readFile("data/Data/kdm_mapdata.bin");
+      expectSHA256Checksum(data, "kdm_mapdata.bin", "8936cbd3787ca041da921e76316259d5a9ad88c7dc737be423fe37c87ec6d700");
+
+      const parsed = new KDM().parse(data).get();
+      const built = new KDM().set(parsed).build();
+
+      expect(data).to.be.deep.equal(built);
+    });
+  });
+});
