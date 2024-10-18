94a9e5974e8a3de7fab3ba65e9d9a7e5acad9a78
Shigu <shiguww@gmail.com>
2024-10-18 22:38:31 +0400

    Reorganize files

---
+++ src/kdm/global/common/padding/kdm-padding.ts
@@ -0,0 +1,30 @@
+import z from "zod";
+import type KDM from "#/kdm/kdm";
+import assert from "node:assert/strict";
+import KDMEntity from "#/kdm/global/common/kdm-entity";
+import type KDMArray from "#/kdm/global/common/array/kdm-array";
+import type KDMStringPointer from "#/kdm/global/common/primitive/kdm-string-pointer";
+
+abstract class KDMPadding extends KDMEntity<never> {
+  public constructor(kdm: KDM) {
+    super(kdm, z.never());
+  }
+
+  public override get strings(): Array<KDMStringPointer> {
+    return [];
+  }
+
+  public override get arrays(): Array<KDMArray> {
+    return [];
+  }
+
+  protected override _get(): never {
+    assert.fail();
+  }
+
+  protected override _set(): never {
+    assert.fail();
+  }
+}
+
+export default KDMPadding;
---
+++ src/kdm/global/common/primitive/kdm-string-pointer-array-pointer.ts
@@ -0,0 +1,122 @@
+import z from "zod";
+import type KDM from "#/kdm/kdm";
+import WBuffer from "#/buffer/w-buffer";
+import assert from "node:assert/strict";
+import type RBuffer from "#/buffer/r-buffer";
+import KDMEntity from "#/kdm/global/common/kdm-entity";
+import type KDMArray from "#/kdm/global/common/array/kdm-array";
+import KDMStringPointer from "#/kdm/global/common/primitive/kdm-string-pointer";
+import KDMStringPointerArray from "#/kdm/global/common/array/kdm-string-pointer-array";
+
+class KDMStringPointerArrayPointer extends KDMEntity<IKDMStringPointerArrayPointer> {
+  private nullTerminatorFlag = false;
+  private _reference: null | string = null;
+
+  public static get schema(): typeof IKDMStringPointerArrayPointer {
+    return IKDMStringPointerArrayPointer;
+  }
+
+  public constructor(kdm: KDM) {
+    super(kdm, IKDMStringPointerArrayPointer);
+  }
+
+  public get reference(): null | string {
+    return this._reference;
+  }
+
+  private set reference(ref: null | string) {
+    this._reference = ref;
+  }
+
+  public override get arrays(): Array<KDMArray> {
+    if (this.reference === null) {
+      return [];
+    }
+
+    return this.array.arrays;
+  }
+
+  public override get sizeof(): number {
+    return WBuffer.U32_SIZE;
+  }
+
+  public override get strings(): Array<KDMStringPointer> {
+    if (this.reference === null) {
+      return [];
+    }
+
+    return this.array.strings;
+  }
+
+  private get array(): KDMStringPointerArray {
+    const array = this.kdm.arrays.find((arr) => arr.refkey === this.reference);
+    assert(array instanceof KDMStringPointerArray);
+
+    return array;
+  }
+
+  protected override _get(): IKDMStringPointerArrayPointer {
+    return IKDMStringPointerArrayPointer.parse({
+      refkey: this.reference
+    });
+  }
+
+  protected override _set(reference: IKDMStringPointerArrayPointer): void {
+    this.reference = reference.refkey;
+  }
+
+  protected override _build(buffer: WBuffer): void {
+    if (this.reference === null) {
+      buffer.setU32(0);
+      return;
+    }
+
+    if(this.nullTerminatorFlag) {
+      this.array.hasNULLTerminator();
+    }
+
+    assert(this.array.offset !== null);
+    buffer.setU32(this.array.offset + KDMStringPointerArray.HEADING_SIZE);
+  }
+
+  protected override _parse(buffer: RBuffer): void {
+    const pointer = buffer.getU32();
+
+    if (pointer === 0) {
+      this.reference = null;
+      return;
+    }
+
+    const array = this.kdm.arrays
+      .find((arr) => arr.offset !== null && ((arr.offset + KDMStringPointerArray.HEADING_SIZE) === pointer));
+
+    assert(array instanceof KDMStringPointerArray);
+
+    if(this.nullTerminatorFlag) {
+      array.hasNULLTerminator();
+    }
+
+    this.reference = array.refkey;
+  }
+
+  public override toJSON(): object {
+    if (this.reference === null) {
+      return ({ ...super.toJSON(), _pointer: null });
+    }
+
+    return ({ ...super.toJSON(), _pointer: this.array.offset });
+  }
+
+  public hasNULLTerminator(): this {
+    this.nullTerminatorFlag = true;
+    return this;
+  }
+}
+
+const IKDMStringPointerArrayPointer = z.object({
+  _kind: z.literal("KDMStringPointerArrayPointer").default("KDMStringPointerArrayPointer"),
+  refkey: z.string().nullable()
+});
+
+type IKDMStringPointerArrayPointer = z.infer<typeof IKDMStringPointerArrayPointer>;
+export default KDMStringPointerArrayPointer;
