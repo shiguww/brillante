8dc97ff67babe3381ada9a571953c7f8eea0ed51
Shigu <shiguww@gmail.com>
2024-10-23 17:48:23 +0400

    Add types

---
+++ src/kdm/switch/switch0.ts
@@ -0,0 +1,49 @@
+import z from "zod";
+import type KDM from "#/kdm/kdm";
+import KDMStruct from "#/kdm/common/kdm-struct";
+import KDMU32 from "#/kdm/common/primitive/kdm-u32";
+import type KDMEntity from "#/kdm/common/kdm-entity";
+import KDMStringPointer from "#/kdm/common/primitive/kdm-string-pointer";
+
+const ISwitch0 = z.object({
+  _kind: z.literal("Switch0").default("Switch0"),
+  unknown0: KDMStringPointer.schema,
+  unknown1: KDMU32.schema
+});
+
+type ISwitch0 = z.infer<typeof ISwitch0>;
+
+class Switch0 extends KDMStruct<ISwitch0> {
+  public static readonly schema = ISwitch0;
+
+  public override readonly unknownSection4Value0 = 0x00000000;
+  public override readonly unknownSection4Value1 = 0x00000000;
+
+  public readonly unknown0 = new KDMStringPointer(this.kdm);
+  public readonly unknown1 = new KDMU32(this.kdm);
+
+  public constructor(kdm: KDM) {
+    super(kdm, ISwitch0);
+  }
+
+  public override get fields(): Array<KDMEntity> {
+    return [
+      this.unknown0,
+      this.unknown1
+    ];
+  }
+
+  protected override _get(): ISwitch0 {
+    return ISwitch0.parse({
+      unknown0: this.unknown0.get(),
+      unknown1: this.unknown1.get()
+    });
+  }
+
+  protected override _set(_switch: ISwitch0): void {
+    this.unknown0.set(_switch.unknown0);
+    this.unknown1.set(_switch.unknown1);
+  }
+}
+
+export default Switch0;
